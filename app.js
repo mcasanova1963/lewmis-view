65fd361252134efa34c1ec0684a9b5a2f0e54e65
mcasanova1963 <miguel.casanova1963@gmail.com>
2026-04-30 15:41:03 -0500

    Update app.js
    sigue sin salir

--- app.js
+++ app.js
@@ -161,7 +161,6 @@ if (Number(box.mode) === 1) {
   document.getElementById("state").innerText = box.state || "-";
 
 const sensorEl = document.getElementById("sensor");
-const demoValue = box.demo_sensor_value || "-";
 
 if (sensorEl) {
   if (demoSensor !== "OFF") {
